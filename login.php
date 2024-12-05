2f6ddec7d00a83e95b1ab0c36b59cc0c1903b766
mano-cymk <mano38617@gmail.com>
2024-12-05 09:30:23 +0545

    [FIX]: full page screenshot enabled + delay enabled

--- login.php
+++ login.php
@@ -7,7 +7,6 @@
         $sql = "select * from login where username = '$username' and password = '$password'";
 
         $result = mysqli_query($conn,  $sql);
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
         $count = mysqli_num_rows($result);
 
         if($count==1){
--- screenshot.php
+++ screenshot.php
@@ -1,7 +1,25 @@
 <?php
+    require 'vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';
+
+    use Spatie\Browsershot\Browsershot;
+
     if(isset($_POST['screenshot'])){
         $url = $_POST["url"];
          
-        echo "You provided the url: ".htmlspecialchars($url);
-    }
+        try{
+            //generate the screenshot
+            Browsershot::url($url)
+            ->setScreenshotType('png')
+            ->setDelay(1000)
+            ->fullpage()
+            ->save('screenshot.png');
+
+            echo "<h3>Screenshot sucessfully generated!,</h3>";
+            echo "<img src='screenshot.png' alt='screenshot of $url' style='max-width:100%;'>";
+            }
+            catch (Exception $e) {
+                echo "Error: ".$e->getMessage();
+            }
+        }
 ?>
